533dd617b6147a58d18280c408d019dad8e6d9af
sunjingsong <jingsong.sun@zoom.us>
2022-01-19 09:36:54 -0800

    render component order

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="question-box-container">
+  <b-jumbotron>
+    <template #lead>
+      {{ currentQuestion.question }}
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button @click="next" variant="success" href="#">
+        Next
+    </b-button>
+  </b-jumbotron>
+ </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    }
+}
+</script>
